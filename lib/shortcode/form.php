28b80c13fc9a3967444925ba8054be89df6f63ee
Guilherme Diego <guilherme.albino.francisco@gmail.com>
2020-06-24 20:02:15 -0300

    fix(shortcode): submit form to hit api resource

--- lib/shortcode/form.php
+++ lib/shortcode/form.php
@@ -69,29 +69,47 @@
         background: #00df91;
         width: 100%;
     }
+
+    .bpc-form > .bpc-form-btn:focus {
+        outline: none;
+    }
+
+    .bpc-form > .bpc-form-btn.bcb-btn-load {
+        position: relative;
+    }
+
+    .bpc-form > .bpc-form-btn.bcb-btn-load::before {
+        content: " ";
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background: rgba(255, 255, 255, 0.3);
+    }
 </style>
 
 <form class="bpc-form">
     <h3 class="bpc-form-title"> <?= $product->name ?> </h3>
     <label class="bpc-form-input">
         <span>Número do Cartão de Crédito</span>
-        <input placeholder="Número do Cartão de Crédito">
+        <input placeholder="Número do Cartão de Crédito" name="cardnumber" autocomplete="cc-number">
     </label>
     <label class="bpc-form-input">
         <span>Nome</span>
-        <input placeholder="Nome">
+        <input placeholder="Nome" name="ccname" autocomplete="cc-name">
     </label>
     <div class="bcp-row">
         <label class="bpc-form-input input-datev">
             <span>Data de Vencimento</span>
-            <input placeholder="Data de Vencimento">
+            <input placeholder="Data de Vencimento" name="cc-exp" autocomplete="cc-exp">
         </label>
         <label class="bpc-form-input input-cv">
             <span>Código de Segurança</span>
-            <input placeholder="Código de Segurança">
+            <input placeholder="Código de Segurança" name="cvc" autocomplete="cc-csc">
         </label>
         <label class="bpc-form-input input-installments">
-            <select>
+            <select name="installments">
                 <?php
                     foreach ($product->installmentObj as $installment) { ?>
                         <option value="<?=$installment['value']?>"><?=$installment['label']?></option>
@@ -107,3 +125,42 @@
         Finalizar Compra
     </button>
 </form>
+
+<script>
+    document.body.onload = function () {
+
+        document.querySelector('.bpc-form').addEventListener('submit', function (e) {
+            e.preventDefault();
+            const loadClass = 'bcb-btn-load';
+            const btn = document.querySelector('.bpc-form-btn')
+            const form = Array
+                .from(e.target.elements)
+                .reduce((prev, el) => ({
+                    ...prev, [el.name]: el.value,
+                }), {
+                    "value":  "<?= $product->value ?>",
+                });
+
+            if (btn.classList.contains(loadClass)) {
+                return;
+            }
+
+            btn.classList.toggle(loadClass);
+
+            if ('fetch' in window ){
+                const options = {
+                    method: 'POST',
+                    body: JSON.stringify(form),
+                    credentials: 'same-origin',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                };
+
+                fetch('/?rest_route/bexs-checkout/v1/pay', options)
+                    .then(() => console.log('ALO'))
+                    .catch(() => btn.classList.toggle(loadClass))
+            }
+        });
+    }
+</script>
